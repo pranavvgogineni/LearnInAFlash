401c9764d74e796333c12855609c1d3f69b7326b
Pranav Gogineni <pranavgv@gmail.com>
2024-07-26 18:49:57 -0400

    initial step in making it query by specific set

--- components/flashcards/flashcard.tsx
+++ components/flashcards/flashcard.tsx
@@ -3,6 +3,6 @@ import {createClient} from "@/utils/supabase/server";
 
 export default async function FlashCards() {
     const supabase = createClient();
-    let { data: flashcards} = await supabase.from('flashcards').select('question, answer')
+    let { data: flashcards} = await supabase.from('flashcards').select('question, answer').eq('set_id', 1);
     return flashcards
 }
