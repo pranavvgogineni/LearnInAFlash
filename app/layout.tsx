541a122028da015e853b86c31a126ebf1e1487f7
Pranav Gogineni <pranavgv@gmail.com>
2024-08-04 19:42:39 -0400

    changes url

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "LearnInAFlash",
+  description: "LearnInAFlash is a flashcard app that helps you learn faster.",
 };
 
 const fontSans = FontSans({
