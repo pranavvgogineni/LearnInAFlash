463750c15e98fc25dc6e982dce2288889e2c7345
Pranav Gogineni <pranavgv@gmail.com>
2024-08-04 22:06:33 -0400

    got rid of something

--- app/flashcards/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProfileForm } from "@/components/shadcn/form";
-
-export default function ProfilePage() {
-    return (
-        <div>
-        <ProfileForm />
-        </div>
-    );
-    }
